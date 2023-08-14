930306b3f249599e82715e51d9267ea6c7bfb832
Dikibe-23 <130453681+Dikibe-23@users.noreply.github.com>
2023-08-14 12:06:39 +0100

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,23 @@
+-- In this SQL work we will be showing the following;
+
+select * from department
+select * from employee
+select * from project
+select * from works_on
+
+--Get the last name of all employees who worked on project 'p3'
+
+select emp_lname from employee
+join works_on
+on employee.emp_no = works_on.emp_no
+where project_no = 'p3'
+
+--Get the first and last name of analyst whose department is located in Seattle
+
+select emp_fname, emp_lname from employee
+join works_on
+on employee.emp_no = works_on.emp_no
+join department
+on department.dept_no = employee.dept_no
+where location = 'seattle'
+and job = 'Analyst'
